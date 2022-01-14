fbacb182f087e5e7d3c5f718763faa350387bc8e
nnavnita <navnitanandakumar@gmail.com>
2022-01-15 00:38:58 +1100

    add args for pp and input

--- src/__main__.py
+++ src/__main__.py
@@ -1,5 +1,16 @@
+import argparse
+
 from vyakarana import svara
 
-inp = input('Enter in Harvard-Kyoto: ')
-v1 = svara.Svara(inp)
-v1.display()
+parser = argparse.ArgumentParser(description='Process Sanksrit Grammar')
+parser.add_argument('-pp', '--pretty', action='store_true', help='pretty print svara details')
+parser.add_argument('svaras', nargs='+', help='svara(s)')
+args = parser.parse_args()
+
+svaras = []
+for sv in args.svaras:
+    svaras.append(svara.Svara(sv))
+
+if args.pretty:
+    for sv in svaras:
+        sv.display()
