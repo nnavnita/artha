a1c6a38dbc41467bb716eca3b1217edec78e5da2
nnavnita <navnitanandakumar@gmail.com>
2022-01-15 00:46:27 +1100

    update readme eg

--- README.md
+++ README.md
@@ -7,4 +7,23 @@ A python model of the Sanskrit language.
 
 ### Vyakarana (Grammar)
 1. Decompose a Svara (Vowel)
-`python3 src`
+```
+python3 src au # svara processed but no output
+python3 src au -pp # svara processed with prettified output
+    Svara: au
+    Kind: compound
+    Breakdown: ['a', 'a', 'u']
+
+python3 src A I U -pp # multiple svara processed with prettified output
+    Svara: A
+    Kind: long
+    Breakdown: ['a', 'a']
+
+    Svara: I
+    Kind: long
+    Breakdown: ['i', 'i']
+
+    Svara: U
+    Kind: long
+    Breakdown: ['u', 'u']
+```
--- src/__main__.py
+++ src/__main__.py
@@ -4,13 +4,13 @@ from vyakarana import svara
 
 parser = argparse.ArgumentParser(description='Process Sanksrit Grammar')
 parser.add_argument('-pp', '--pretty', action='store_true', help='pretty print svara details')
-parser.add_argument('svaras', nargs='+', help='svara(s)')
+parser.add_argument('svara', nargs='+', help='svara')
 args = parser.parse_args()
 
-svaras = []
-for sv in args.svaras:
-    svaras.append(svara.Svara(sv))
+sv = []
+for s in args.svara:
+    sv.append(svara.Svara(s))
 
 if args.pretty:
-    for sv in svaras:
-        sv.display()
+    for s in sv:
+        s.display()
